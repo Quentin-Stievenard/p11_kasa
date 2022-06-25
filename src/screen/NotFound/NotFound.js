e3f3604f62ceee3e740e4178f07f0ca298bf1264
Quentin Stievenard <qstievenard@gmail.com>
2022-06-25 12:20:03 +0200

    [RESPONSIVE] NotFound page

--- src/screen/NotFound/NotFound.js
+++ src/screen/NotFound/NotFound.js
@@ -5,22 +5,24 @@ import './NotFound.scss';
 
 // components
 import Header from '../../components/Header/Header';
+import Footer from '../../components/Footer/Footer';
 
 export default function NotFound() {
   return (
     <div className="not-found-container">
-      <main>
+      <main className="mb-28">
         <Header />
-        <div className="flex flex-col justify-center items-center">
+        <div className="flex flex-col justify-center items-center mt-24">
           <p className="not-found-title">404</p>
           <p className="not-found-text">
             Oups! La page que vous demandez n&apos;existe pas.
           </p>
-          <a href="/" className="underline">
+          <a href="/" className="link-notFound">
             Retourner sur la page d’accueil
           </a>
         </div>
       </main>
+      <Footer />
     </div>
   );
 }
